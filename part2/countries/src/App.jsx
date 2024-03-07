ff4250b844c62d268b441b4a3fa92aadcab93d55
daonv223 <dao.nv223@gmail.com>
2024-03-07 13:19:30 +0700

    edit countries excersise

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 const Weather = ({ weather }) => {
-  if (weather === undefined) return null
+  if (weather === null) return null
   else return (
     <>
       <p>temperature {Math.round((weather.main.temp - 273.15) * 100)/100} Celcius</p>
@@ -25,14 +25,13 @@ const Country = ({ country }) => {
       </ul>
       <img src={country.flags.png} alt={country.flags.alt} />
       <h2>Weather in {country.capital[0]}</h2>
-      <Weather weather={country.weather} />
     </div>
   )
 }
 
 const Countries = ({ countries, onShowCountry }) => {
   if (countries.length > 10) return <p>Too many matches, specify another filter</p>
-  if (countries.length === 1) {
+  if (countries.length <= 1) {
     return null
   }
   return (
@@ -47,6 +46,7 @@ function App() {
   const [allCountries, setAllCountries] = useState([])
   const [countries, setCountries] = useState([])
   const [country, setCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
 
   useEffect(() => {
     axios
@@ -57,8 +57,11 @@ function App() {
   useEffect(() => {
     const result = query === '' ? [] : allCountries.filter(country => country.name.common.toLowerCase().includes(query.toLowerCase()))
     setCountries(result)
-    if (result.length === 1) setCountry(result[0])
-    else setCountry(null)
+    if (result.length === 1) {
+      setCountry(result[0])
+    } else {
+      setCountry(null)
+    }
   }, [query, allCountries])
 
   useEffect(() => {
@@ -66,11 +69,13 @@ function App() {
       axios
         .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${import.meta.env.VITE_WEATHER_KEY}`)
         .then(response => {
-          setCountry({ ...country, weather: response.data })
+          setWeather(response.data)
         })
         .catch(error => {
           console.log(error.message)
         })
+    } else {
+      setWeather(null)
     }
   }, [country])
 
@@ -83,6 +88,7 @@ function App() {
       find countries <input value={query} onChange={event => setQuery(event.target.value)}/>
       <Countries countries={countries} onShowCountry={handleShowCountry} />
       <Country country={country} />
+      <Weather weather={weather} />
     </div>
   )
 }

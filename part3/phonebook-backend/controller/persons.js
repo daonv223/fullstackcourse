5f06c15ae0d51afeb46fe31c7156449b31283955
daonv223 <dao.nv223@gmail.com>
2024-07-18 15:59:18 +0700

    part4: exercises 4.13-4.14

---
+++ part3/phonebook-backend/controller/persons.js
@@ -0,0 +1,8 @@
+const personsRouter = require('express').Router()
+const Person = require('../models/person')
+
+personsRouter.get('/', (request, response) => {
+
+})
+
+module.exports = personsRouter
